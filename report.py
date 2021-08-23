89ea2ee9e82ace2bd9d4ad01d616c967dfbe9d64
TrT-TOT <35398009+TrT-TOT@users.noreply.github.com>
2021-08-23 14:58:45 +0800

    Update report.py

--- report.py
+++ report.py
@@ -9,8 +9,11 @@ import sys
 import argparse
 import PIL
 import io
-import tesseract
-from PIL import Image
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
 from bs4 import BeautifulSoup
 
 class Report(object):
