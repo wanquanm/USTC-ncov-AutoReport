7d0830944ef1d85f78cc9d4e4107fa2a075bcaaf
TrT-TOT <35398009+TrT-TOT@users.noreply.github.com>
2021-08-23 14:56:17 +0800

    Update report.py

--- report.py
+++ report.py
@@ -21,7 +21,7 @@ class Report(object):
 
     def report(self):
         loginsuccess = False
-        retrycount = 5
+        retrycount = 2
         while (not loginsuccess) and retrycount:
             session = self.login()
             cookies = session.cookies
