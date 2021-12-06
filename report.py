f942717210b9acc09918b5b70faf7403708462eb
TrT-TOT <35398009+TrT-TOT@users.noreply.github.com>
2021-12-06 15:53:15 +0800

    Update report.py

--- report.py
+++ report.py
@@ -70,7 +70,7 @@ class Report(object):
         }
 
         url1 = "https://weixine.ustc.edu.cn/2020/apply/daliy/post"
-        resp1=session.post(url1, data=data1, headers=headers1)
+        resp1=session.post(url1, data=data1, headers=header1)
         print(resp1.status_code)
         if resp1.status_code != 200:
             print("error")
