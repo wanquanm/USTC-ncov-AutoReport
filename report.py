35af5d1c7477c64110a5e5b5b1b21b622ee16929
TrT-TOT <35398009+TrT-TOT@users.noreply.github.com>
2021-12-21 16:25:02 +0800

    Update report.py

--- report.py
+++ report.py
@@ -92,12 +92,11 @@ class Report(object):
         if(today == 2):
             getform = session.get("https://weixine.ustc.edu.cn/2020/apply/daliy")
             data = getform.text
-            print(data)
             data = data.encode('ascii','ignore').decode('utf-8','ignore')
             soup = BeautifulSoup(data, 'html.parser')
             token = soup.find("input", {"name": "_token"})['value']
-            start_date = soup.find("input", {"id": "start_date"})['value']
-            end_date = soup.find("input", {"id": "end_date"})['value']
+            start_date = datetime.now().strftime('%Y-%m-%d')
+            end_date = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d')
 
             url = "https://weixine.ustc.edu.cn/2020/apply/daliy/post"
             data2={}
@@ -107,8 +106,6 @@ class Report(object):
 
             post_data=session.post(url, data=data2, headers=headers)
             data = session.get("https://weixine.ustc.edu.cn/2020/apply_total?t=d").text
-            print("=======================")
-            print(data)
 
             soup = BeautifulSoup(data, 'html.parser')
             date = soup.find(text=pattern)
